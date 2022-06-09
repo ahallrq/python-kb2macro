9badebea4c0f3ecb8a3be80f84d8a15018eb163c
Andrew Hall <ahall@redquasar.xyz>
2022-06-09 23:36:22 +1000

    Implement basic M_TYPE macro, add debug setting to MacroDevice

--- macro.py
+++ macro.py
@@ -1,6 +1,9 @@
+from curses import KEY_DOWN
 from enum import Enum
+import random
 import subprocess, shlex
 import sys
+import time
 from typing import Any, Callable
 import evdev
 import pyautogui
@@ -20,17 +23,21 @@ class KeyState(Enum):
     K_UP = 0
     K_DOWN = 1
     K_HOLD = 2
+    K_PRESS = 99  # technically not a keystate but used for pyautogui for a combined down/up keypress
 
 
 class Macro:
-    def __init__(self, name: str, type: MacroType, value: str | Callable):
+    def __init__(
+        self, name: str, type: MacroType, value: str | Callable, args: dict = {}
+    ):
         self.name = name
         self.macro_type = type
         self.macro_value = value
+        self.macro_args = args
 
-    def __call__(self):
+    def __call__(self, *args, **kwargs):
         """When the object is called this will attempt to run the method with the same name as the macro type"""
-        getattr(self, self.macro_type.name)()
+        getattr(self, self.macro_type.name)(*args, **kwargs)
 
     def M_PRINT(self):
         """Type a block of text onto the keyboard"""
@@ -38,7 +45,23 @@ class Macro:
 
     def M_TYPE(self):
         """Type a series of keypresses including control keys."""
-        pass
+        # TODO: In the future, improve this method such that the pyautogui calls are generated \
+        #   when the macro is registered rather than parsing macro_value each time.
+        delay = self.macro_args.get("delay", 0.01)
+        delay_randomness = self.macro_args.get("delay_randomness", 0)
+
+        for k in self.macro_value:
+            (key, state) = k
+            match state:
+                case KeyState.K_PRESS:
+                    pyautogui.press(key)
+                case KeyState.K_DOWN:
+                    pyautogui.keyDown(key)
+                case KeyState.K_UP:
+                    pyautogui.keyUp(key)
+            time.sleep(
+                delay + round(random.uniform(-delay_randomness, delay_randomness), 2)
+            )
 
     def M_SHELL(self):
         """Run a shell command and type the output."""
@@ -59,7 +82,7 @@ class Macro:
 
 
 class MacroDevice:
-    def __init__(self, evdev_path: str, grab=True):
+    def __init__(self, evdev_path: str, grab=True, debug=False):
         self.__macros = {}
 
         try:
@@ -69,6 +92,8 @@ class MacroDevice:
                 self.__evdev.grab()
         except Exception as e:
             sys.exit(f"Failed to open evdev device: {e}")
+
+        self.debug = debug
 
     def register_macro(
         self,
@@ -108,6 +133,9 @@ class MacroDevice:
         for ev in self.__evdev.read_loop():
             if ev.type != evdev.ecodes.EV_KEY:
                 continue
+
+            if self.debug:
+                print(evdev.categorize(ev))
 
             if macro := self.__macros.get(ev.code, {}).get(
                 KeyState(ev.value).name, None
--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import macro
 
 # udev rule: SUBSYSTEM=="input", ATTRS{id/vendor}=="xxxx", ATTRS{id/product}=="yyyy", MODE="0660", GROUP="somegroup", SYMLINK+="input/macroN"
 
-EVDEV_DEVICE = "/dev/input/macro0"
+EVDEV_DEVICE = "/dev/input/macro1"
+DEBUG_KEYS = False
 
 
 def main():
-    m = macro.MacroDevice(EVDEV_DEVICE, grab=True)
+    m = macro.MacroDevice(EVDEV_DEVICE, grab=True, debug=DEBUG_KEYS)
     m.register_macro(
         ecodes.KEY_KP0,
         macro.KeyState.K_DOWN,
@@ -25,6 +26,33 @@ def main():
         ecodes.KEY_KPPLUS,
         macro.KeyState.K_DOWN,
         macro.Macro("run a program", macro.MacroType.M_EXEC, "/usr/bin/kwrite"),
+    )
+    m.register_macro(
+        ecodes.KEY_KPMINUS,
+        macro.KeyState.K_DOWN,
+        macro.Macro(
+            "press a series of keys",
+            macro.MacroType.M_TYPE,
+            [
+                ("shift", macro.KeyState.K_DOWN),
+                ("H", macro.KeyState.K_PRESS),
+                ("shift", macro.KeyState.K_UP),
+                ("e", macro.KeyState.K_PRESS),
+                ("l", macro.KeyState.K_PRESS),
+                ("l", macro.KeyState.K_PRESS),
+                ("o", macro.KeyState.K_PRESS),
+                ("space", macro.KeyState.K_PRESS),
+                ("shift", macro.KeyState.K_DOWN),
+                ("w", macro.KeyState.K_PRESS),
+                ("shift", macro.KeyState.K_UP),
+                ("o", macro.KeyState.K_PRESS),
+                ("r", macro.KeyState.K_PRESS),
+                ("l", macro.KeyState.K_PRESS),
+                ("d", macro.KeyState.K_PRESS),
+                ("!", macro.KeyState.K_PRESS),
+            ],
+            {"delay": 0.100},
+        ),
     )
 
     m.event_loop()
